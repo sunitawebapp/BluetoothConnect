7694ad4179872a3715cae93a7e8097d44f4640c4
Sunita Chowdhury <sunita.chowdhury@weavers-web.com>
2023-01-09 17:36:01 +0530

    updated 2023.01.09

--- app/src/main/java/com/example/bluetoothconnect/SharePreferrence/AppController.kt
+++ app/src/main/java/com/example/bluetoothconnect/SharePreferrence/AppController.kt
@@ -3,17 +3,13 @@ package com.example.bluetoothconnect.SharePreferrence
 import android.app.Application
 import android.content.Intent
 import android.widget.Toast
-import com.example.bluetoothconnect.MessageService
 
 class AppController() : Application() {
-var text="sunita"
+
     override fun onCreate() {
         super.onCreate()
 
         SessionManager(applicationContext)
-        var intent=Intent(this,MessageService::class.java)
-        intent.setAction(text)
-        startService(intent)
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
     }
 }
